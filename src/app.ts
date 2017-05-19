e0a492379e551d2b273cea0ab02bcf6da68d5ce9
Brian Chu <brianchu@D4331.NET>
2017-05-19 10:57:39 +0800

    init

---
+++ src/app.ts
@@ -0,0 +1,68 @@
+declare global {
+    interface Date {
+        yyyyMMddHHmmss(): string;
+    }
+}
+
+Date.prototype.yyyyMMddHHmmss = function () {
+    var date = this;
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    var day = date.getDate();
+    var hh = date.getHours();
+    var mm = date.getMinutes();
+    var ss = date.getSeconds();
+
+    return "" + year +
+        (month < 10 ? "0" + month : month) +
+        (day < 10 ? "0" + day : day) +
+        (hh < 10 ? "0" + hh : hh) +
+        (mm < 10 ? "0" + mm : mm) +
+        (ss < 10 ? "0" + ss : ss);
+};
+
+import * as mqtt from "mqtt";
+
+let client = mqtt.connect("wss://mbltest01.mqtt.iot.gz.baidubce.com:8884/mqtt", {
+    username: "mbltest01/mind",
+    password: "LUYHdof9is/Cg7XJMC/Nq8Tlk+tNyFrrBPnM9fw4n9Q="
+});
+
+client.on("connect", (connack) => {
+    console.log("on connect");
+    console.log(JSON.stringify(connack));
+
+    client.on("message", (topic, message, packet) => {
+        console.log("on message");
+        console.log(JSON.stringify(topic));
+        console.log(JSON.stringify(message));
+        console.log(JSON.stringify(packet));
+
+        console.log(topic + ": " + message.toString());
+    });
+
+    client.subscribe("fromEight/#", (err, granted) => {
+        console.log("subscribe");
+        console.log(JSON.stringify(err));
+        console.log(JSON.stringify(granted));
+    });
+
+    client.subscribe("mind/#", (err, granted) => {
+        console.log("subscribe");
+        console.log(JSON.stringify(err));
+        console.log(JSON.stringify(granted));
+
+        if ((typeof err === "undefined" || err === null) && granted.some(value => value.topic === "mind/#" && value.qos !== 128)) {
+            let data: any = {
+                who: "mind",
+                whoAmI: "mind",
+                when: new Date().yyyyMMddHHmmss()
+            };
+
+            client.publish("toEight/i.am", JSON.stringify(data), (err) => {
+                console.log("publish");
+                console.log(JSON.stringify(err));
+            });
+        }
+    });
+})
